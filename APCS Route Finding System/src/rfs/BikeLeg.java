a283b369242df9605d048447efae0f53aeeb40eb
Colin Johnson <colin@colinjohnson.net>
2017-04-21 09:28:34 -0700

    BIKE AND TRUCK
    constructors and comments

--- APCS Route Finding System/src/rfs/BikeLeg.java
+++ APCS Route Finding System/src/rfs/BikeLeg.java
@@ -3,15 +3,14 @@ package rfs;
 public class BikeLeg extends Leg{
 
 	/**
-	 * Origin and destination must be TruckDepot and BikeDeliveryStation or two
+	 * A route leg for bikes. Origin and destination must be TruckDepot and BikeDeliveryStation or two
 	 * BikeDeliveryStations.
 	 * @param origin
 	 * @param distanceInKm The length of the Leg in kilometers             
-	 * @param days String representing the days of the week this Leg runs. 
-	 * @param days 
+	 * @param days String representing the days of the week this Leg runs.  
 	 */
 	public BikeLeg(Location origin, Location destination, int distanceInKm, String days) {
 		super(origin, destination, distanceInKm, days, 1);
-		// TODO Auto-generated constructor stub
+
 	} // BikeLeg Constructor
 } // BikeLeg
