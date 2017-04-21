bb3b2082087fdcd55a3042bb94f950bacace22d7
josefwaller <josef@siriusapplications.com>
2017-04-21 09:49:31 -0700

    Implemented Flight class and constructor.

--- APCS Route Finding System/src/rfs/Flight.java
+++ APCS Route Finding System/src/rfs/Flight.java
@@ -1,5 +1,19 @@
 package rfs;
 
-public class Flight {
+public class Flight extends Leg {
 
-}
+	/*
+	 * <code>Flight</code>
+	 * 
+	 * @param origin: The origin of the flight
+	 * @param destination: The destination of the flight
+	 * @param distanceInKm: The distance of the flight, in kilometers
+	 * @param days: The days of the week when the flight is available
+	 */
+	public Flight(Location origin, Location destination, int distanceInKm, String days) {
+		
+		super(origin, destination, distanceInKm, days, 0.23);
+		
+	} // FlightConstructor
+	
+} // Flight
