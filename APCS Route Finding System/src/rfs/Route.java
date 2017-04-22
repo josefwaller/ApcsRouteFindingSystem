437aadb9722747effd872b629010571c4a9a78e2
ColinGJohnson <colin@colinjohnson.net>
2017-04-22 01:49:13 -0700

    Basic Route Functionality
    Route superclass mostly completed, but not it's subclasses.

--- APCS Route Finding System/src/rfs/Route.java
+++ APCS Route Finding System/src/rfs/Route.java
@@ -1,5 +1,58 @@
 package rfs;
 
-public class Route {
+import java.util.ArrayList;
 
-}
+public class Route {
+	ArrayList<Leg> connectedLegs = new ArrayList<>();
+	
+	public Route(ArrayList<Leg> L){
+		this.connectedLegs = L;
+	} // Route Constructor
+	
+	/**
+	 * Calculates the total cost of traversing this route.
+	 * @return The total cost of this route.
+	 */
+	public double totalCost(){
+		double totalCost = 0;
+		for (Leg leg : connectedLegs) {
+			totalCost += leg.getCostPerKm()*leg.getDistance();
+		}
+		return totalCost;
+	} // totalCost
+	
+	/**
+	 * Calculates the total distance covered by this route.
+	 * @return The total length of this route in kilometers.
+	 */
+	public int totalDistance(){
+		int totalDistance = 0;
+		for (Leg leg: connectedLegs){
+			totalDistance += leg.getDistance();
+		}
+		return totalDistance;
+	} // totalDistance
+	
+	/**
+	 * @return The total number of legs in this route.
+	 */
+	public int totalSteps(){
+		return connectedLegs.size();
+	} // totalSteps
+	
+	/**
+	 * The adds a new leg onto the end of this route
+	 * @param newLeg The <code>Leg</code> to be added.
+	 */
+	public void addLeg(Leg newLeg){
+		
+		// make sure the new Leg connects to the end of this route
+		if (newLeg.getOrigin().getName() == connectedLegs.get(connectedLegs.size() - 1).getDestination().getName()) {
+			
+			// add the new Leg
+			connectedLegs.add(newLeg);
+		} else {
+			System.err.println("Invalid Route addition.");
+		}
+	} // addLeg
+} // Route
